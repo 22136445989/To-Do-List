407f508f1c807c971bf6c92d4b2111a53fc75c00
1234445666666 <p.k.270607@gmail.com>
2025-02-14 09:43:53 +0300

    pull request

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,23 @@ const inputBox = document.getElementById("input-box");
 let secretB = document.getElementById("discard");
 const listContainer = document.getElementById("list-container");
 
-function addTask() {
+let addTask = document.querySelector(".add");
+
+addTask.addEventListener("click", function () {
   if (inputBox.value === "") {
     alert("Write something!");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
-    let span = document.createElement("span"    );
+    let span = document.createElement("span");
     span.innerHTML = "х";
     li.appendChild(span);
   }
   inputBox.value = "";
   saveData();
-  
-}
+  checkContainer();
+});
 
 listContainer.addEventListener("click", function (joinup) {
   if (joinup.target.tagName === "LI") {
@@ -30,11 +32,12 @@ listContainer.addEventListener("click", function (joinup) {
   }
 });
 
-function deleteAll() {
+let deleteAllBtn = document.querySelector(".udalit");
+deleteAllBtn.addEventListener("click", function () {
   listContainer.innerHTML = "";
-}
-saveData();
-checkContainer();
+  saveData();
+  checkContainer();
+});
 
 function checkContainer() {
   if (listContainer.children.length > 0) {
