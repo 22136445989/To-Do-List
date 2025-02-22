38cff6d66fc7d8bd8c17bd39c7d37ab79c8a0e80
22136445989 <supremenestor@gmail.com>
2025-02-23 00:59:07 +0300

    Functions-Fixed

--- scripts/main.js
+++ scripts/main.js
@@ -2,42 +2,42 @@ const inputBox = document.getElementById("input-box");
 let secretB = document.getElementById("discard");
 const listContainer = document.getElementById("list-container");
 let addTask = document.querySelector(".add");
-let deleteAll = document.querySelector(".udalit")
+let deleteAll = document.querySelector(".udalit");
 
-addTask.addEventListener("click", function() {
+addTask.addEventListener("click", AddTask);
 
+function AddTask() {
   if (inputBox.value === "") {
     alert("Write something!");
-
   } else {
     let li = document.createElement("li");
-
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+
     let span = document.createElement("span");
     span.innerHTML = "х";
     li.appendChild(span);
   }
   inputBox.value = "";
   saveData();
+  checkForListItems();
+}
 
-})
+listContainer.addEventListener("click", highLightList);
 
-
-
-listContainer.addEventListener("click", function (joinup) {
+function highLightList(joinup) {
   if (joinup.target.tagName === "LI") {
     joinup.target.classList.toggle("checked");
     saveData();
-    checkContainer();
+    checkForListItems();
   } else if (joinup.target.tagName === "SPAN") {
     joinup.target.parentElement.remove();
     saveData();
+    checkForListItems();
   }
-});
+}
 
-
-function checkContainer() {
+function checkForListItems() {
   if (listContainer.children.length > 0) {
     secretB.style.display = "block";
   } else {
@@ -46,19 +46,12 @@ function checkContainer() {
 }
 saveData();
 
+deleteAll.addEventListener("click", discardAll);
 
-deleteAll.addEventListener("click", function(){
+function discardAll() {
   listContainer.innerHTML = "";
-  checkContainer();
-})
-
-
-// function deleteAll() {
-//   listContainer.innerHTML = "";
-//   checkContainer();
-// }
-// saveData();
-
+  checkForListItems();
+}
 
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
@@ -68,5 +61,3 @@ function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
-showTask();
-
